b0052e54ba8111776f325f97af6faaf0c13edbe6
Matt Barson <matt.barson@gmail.com>
2020-12-31 00:45:57 +0000

    delete rows instead of delete row.

--- grandclock/check_chime.py
+++ grandclock/check_chime.py
@@ -358,7 +358,7 @@ class ArchiveManager:
         post_to_sheets = PostToSheets(sheet_name, sheet_id)
         values = post_to_sheets.sheet.get_all_values()[1:]  # Skip header.
         rows_to_remove = len(values) - (30 * 24)
-        post_to_sheets.send_it(post_to_sheets.sheet.delete_rows, limit=5, start_index=2, end_index=rows_to_remove)
+        post_to_sheets.send_it(post_to_sheets.sheet.delete_rows, limit=5, start_index=2, end_index=rows_to_remove+2)
 
 
 def main():
