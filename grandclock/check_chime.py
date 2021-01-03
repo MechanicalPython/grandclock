854f93b0bfd5ea996e43e283e0e024222af53499
Matt Barson <matt.barson@gmail.com>
2021-01-03 08:31:22 +0000

    Python 3.7 version as pi will not install scipy

--- grandclock/check_chime.py
+++ grandclock/check_chime.py
@@ -361,9 +361,9 @@ class ArchiveManager:
         post_to_sheets = PostToSheets(sheet_name, sheet_id)
         values = post_to_sheets.sheet.get_all_values()[1:]  # Skip header.
         rows_to_remove = len(values) - (30 * 24)
-        post_to_sheets.send_it(post_to_sheets.sheet.delete_rows, limit=3, start_index=2, end_index=rows_to_remove)
-        # for i in range(2, rows_to_remove + 2):  # +2 for header and index start at 1.
-        #     post_to_sheets.send_it(post_to_sheets.sheet.delete_row, limit=2, index=2)
+        # post_to_sheets.send_it(post_to_sheets.sheet.delete_rows, limit=3, start_index=2, end_index=rows_to_remove)
+        for i in range(2, rows_to_remove + 2):  # +2 for header and index start at 1.
+            post_to_sheets.send_it(post_to_sheets.sheet.delete_row, limit=2, index=2)
 
 
 def main():
